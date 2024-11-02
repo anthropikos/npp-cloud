860367838cc27b4d5743531309f4fa5e0491c820
Anthony Lee <anthony8lee@gmail.com>
2024-11-02 08:11:27 -0700

    Added MacOS custom attribute file.

--- tf-mod2-demo1/provider.tf
+++ tf-mod2-demo1/provider.tf
@@ -1,9 +1,9 @@
 
 //https://registry.terraform.io/providers/hashicorp/google/latest/docs/guides/provider_reference
 provider "google" {
-  credentials = file("FILL-IN.json")
+  credentials = file("~/cloud-networking-course-2323de37b6d1.json")
 
-  project = "FILL-IN"
+  project = "cloud-networking-course"
   region  = "us-central1"
   zone    = "us-central1-c"
 }
